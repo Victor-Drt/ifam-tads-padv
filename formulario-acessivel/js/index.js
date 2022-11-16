5ddd1b5e06749ed4b6589677109ec68299b73a6e
Victor-Drt <victorduarte778@gmail.com>
2022-11-16 12:06:26 -0400

    add: config estatica de veloc e ling da narracao

--- formulario-acessivel/js/index.js
+++ formulario-acessivel/js/index.js
@@ -1,26 +1,38 @@
-
+// metodo que le todos os rotulos a partir da passado no parametro
 function lerTodosRotulos(rotulo) {
-    showAndSayIt($(rotulo).text()); // metodo para falar
+    showAndSayIt($(rotulo).text()); 
 }
 
+// metodo que le o valor do campo a partir do valor passado no parametro
 function lerValor(rotuloValor) {
     var nome = $(rotuloValor).val();
     showAndSayIt(nome); // metodo para falar
 }
 
+// metodo que le o rotulo do campo a partir do valor passado no parametro
 function lerRotulo(nomeRotulo) {
     var textoPraLer = $(nomeRotulo).text();
     showAndSayIt(textoPraLer);
 }
 
+
+// metodo que le o corpo do html inteiro com excessao dos valores de input
 function lerTudo() {
     var conteudo = $('body').text();
     showAndSayIt(conteudo);
 
 }
 
+// metodo que configura e executa a narração
 const showAndSayIt = (m) => {
+    // instancia a api de voz
     const msg = new SpeechSynthesisUtterance(m);
-    msg.onstart = () => console.log(m);
-    speechSynthesis.speak(msg);
+
+    // configura a linguagem e velocidade da narracao
+    msg.lang = 'pt-br';
+    msg.rate = 0.7;
+
+    msg.onstart = () => console.log(m); // vai escrever no console a mensagem lida
+
+    speechSynthesis.speak(msg); // aqui ocorre a narração
 }
